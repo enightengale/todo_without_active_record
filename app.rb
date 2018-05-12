3f04bd4c9023a878156a6013ebd2174eb3ac4272
Elijah Nightengale <elijah.charles.smile@gmail.com>
2018-05-11 21:22:27 -0400

    add a form that posts to / to create a new list

--- app.rb
+++ app.rb
@@ -1,12 +1,21 @@
 require("sinatra")
 require("sinatra/reloader")
 require("./lib/list")
-require("pg")
 also_reload('lib/**/*.rb')
+require("pg")
+
 
 DB = PG.connect({:dbname => "to_do_without_active_record"})
 
 get("/") do
+  @lists = List.all()
+  erb(:index)
+end
+
+post("/") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
   @lists = List.all()
   erb(:index)
 end
